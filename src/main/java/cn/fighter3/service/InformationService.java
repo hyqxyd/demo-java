9590683c3f337d3be18ce774afd89aa980aab87a
侯发志 <11311509+hou-fazhi@user.noreply.gitee.com>
2025-01-16 11:33:01 +0000

    删除文件 src

--- src/main/java/cn/fighter3/service/InformationService.java
+++
@@ -1,9 +0,0 @@
-package cn.fighter3.service;
-
-import cn.fighter3.dto.QuestionDetail;
-
-import java.util.List;
-
-public interface InformationService {
-    List<QuestionDetail> getUserQuestionsAndAnswers(int userId);
-}
